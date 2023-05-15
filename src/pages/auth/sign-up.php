24a4ab194483a6f53bd9f1d8af4dea7a5ac2f146
zaamaulan <zaamaulan@gmail.com>
2023-05-16 01:05:29 +0700

    menambahkan sign up setengah jadi

--- src/pages/auth/sign-up.php
+++ src/pages/auth/sign-up.php
@@ -20,26 +20,22 @@
 
 <body class="bg-stone-50 font-inter">
   <div class="flex justify-center items-center h-screen ">
-    <form action="" method="post" class="flex flex-col bg-white px-20 py-12 rounded-md w-[24%]">
-      <h1 class="text-4xl text-center mb-10 font-semibold">Sign In</h1>
-      <div class="mb-6 text-md">
+    <form action="sign-up-scnd.php" method="post" class="flex flex-col bg-white px-24 py-14 rounded-md">
+      <h1 class="text-4xl text-center mb-10 font-semibold">Sign Up</h1>
+      <div class="mb-6 text-md w-full mx-auto">
         <label for="username">Username</label>
-        <input type="text" name="username" id="" class="border-2 border-gray-200 focus:outline-2 focus:outline-indigo-500 px-2 py-1 rounded-lg w-full" require>
+        <input type="text" name="username" id="" class="border-2 border-gray-200 focus:outline-2 focus:outline-indigo-500 px-3 py-1.5 rounded-lg w-full" require>
       </div>
-      <div class="mb-6 text-md">
+      <div class="mb-6 text-md w-full mx-auto">
         <label for="email">Email</label>
-        <input type="email" name="email" id="" class="border-2 border-gray-200 focus:outline-2 focus:outline-indigo-500 px-2 py-1 rounded-lg w-full" require>
+        <input type="email" name="email" id="" class="border-2 border-gray-200 focus:outline-2 focus:outline-indigo-500 px-3 py-1.5 rounded-lg w-full" require>
       </div>
-      <div class="mb-6 text-md">
-        <label for="password">Password</label>
-        <input type="password" name="password" id="" class="border-2 border-gray-200 focus:outline-2 focus:outline-indigo-500 px-2 py-1 rounded-lg w-full" require>
+      <div class="mb-3 text-md w-full mx-auto">
+        <label for="no_whatsapp">Nomor WhatsApp</label>
+        <input type="number" name="no_whatsapp" id="" class="border-2 border-gray-200 focus:outline-2 focus:outline-indigo-500 px-3 py-1.5 rounded-lg w-full" require>
       </div>
-      <div class="mb-3 text-md">
-        <label for="no_whatsapp">Password</label>
-        <input type="password" name="password" id="" class="border-2 border-gray-200 focus:outline-2 focus:outline-indigo-500 px-2 py-1 rounded-lg w-full" require>
-      </div>
-      <h1 class="text-sm text-right mb-10">Belum Punya Akun? <a href="sign-up.php" class="text-indigo-600 ">Sign Up</a></h1>
-      <input type="submit" value="Sign In" class="px-6 py-2.5 bg-indigo-500 text-white font-semibold hover:bg-indigo-600 transition rounded-lg">
+      <h1 class="text-sm text-right mb-10">Sudah Punya Akun? <a href="sign-in.php" class="text-indigo-600 ">Sign In</a></h1>
+      <input type="submit" value="Sign Up" class="px-6 py-2.5 bg-indigo-500 text-white font-semibold hover:bg-indigo-600 transition rounded-lg w-full mx-auto">
     </form>
   </div>
 </body>
